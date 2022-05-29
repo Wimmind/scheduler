6b4aa2f90e2e1e0b27334a9f7b8c953751627f09
Wimmind <terehov.anton@bk.ru>
2022-05-29 21:17:23 +0400

    refactor

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const state = {
   users: [],
   currentFirstDate: null
 }
+const NUMBER_DAYS_OF_WEEK = 7
+const BASE_URL = `https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/`
+
 const THS = document.querySelector('#thead-row').children;
 const BACKLOG = document.querySelector('#backlog__container')
 const SCHEDULER_TABLE_BODY = document.querySelector('#scheduler-table_tbody')
 
-const NUMBER_DAYS_OF_WEEK = 7
-
 document.querySelector('#prev-week').onclick = function () {
-  setDate(addDate(state.currentFirstDate, -NUMBER_DAYS_OF_WEEK));
+  setWeek(changeDayDate(state.currentFirstDate, -NUMBER_DAYS_OF_WEEK));
   updateCells()
 };
 document.querySelector('#next-week').onclick = function () {
-  setDate(addDate(state.currentFirstDate, NUMBER_DAYS_OF_WEEK));
+  setWeek(changeDayDate(state.currentFirstDate, NUMBER_DAYS_OF_WEEK));
   updateCells()
 };
 
 BACKLOG.addEventListener('dragstart', e => {
   e.target.classList.add('selected');
-})
+});
 
 BACKLOG.addEventListener('dragend', e => {
   e.target.classList.remove('selected');
-})
-
+});
 
 SCHEDULER_TABLE_BODY.addEventListener('dragover', e => {
   e.preventDefault();
@@ -36,14 +36,14 @@ SCHEDULER_TABLE_BODY.addEventListener('drop', e => {
 
   const draggableTask = BACKLOG.querySelector('.selected')
   const task = state.backlog.find(item => item.id === draggableTask.getAttribute('id'))
-  const currentElement = e.target.closest('td')
-  const typeElement = currentElement.getAttribute('type')
+  const currentCell = e.target.closest('td')
+  const typeCell = currentCell.getAttribute('type')
 
-  const isTableCell = typeElement === 'task' || typeElement === 'username'
+  const isTableCell = typeCell === 'task' || typeCell === 'username'
   if (!isTableCell) return
 
-  const userIndex = state.users.findIndex(item => item.id === parseInt(currentElement.getAttribute('userId')))
-  const currentDate = typeElement === 'task' ? currentElement.getAttribute('date') : task.planStartDate
+  const userIndex = state.users.findIndex(item => String(item.id) === currentCell.getAttribute('user-id'))
+  const currentDate = typeCell === 'task' ? currentCell.getAttribute('date') : task.planStartDate
   const activeDate = state.users[userIndex].tasks[currentDate]
   if (activeDate) {
     if (activeDate.length === 3) {
@@ -58,40 +58,14 @@ SCHEDULER_TABLE_BODY.addEventListener('drop', e => {
   state.backlog = state.backlog.filter(item => item.id !== draggableTask.getAttribute('id'))
   updateCells()
   updateBacklog()
-})
-
-function addDate(date, n) {
-  date.setDate(date.getDate() + n);
-  return date;
-};
-
-function setDate(date) {
-  const week = date.getDay() - 1;
-  date = addDate(date, week * -1);
-  state.currentFirstDate = new Date(date);
-
-  Array.from(THS).forEach((th, i) => {
-    if (i === 0) return
-
-    const curDate = i === 1 ? date : addDate(date, 1)
-    const month = parseNumber(curDate.getMonth() + 1)
-    const day = parseNumber(curDate.getDate())
-    const year = date.getFullYear()
-    th.innerHTML = `${day}.${month}`
-    th.setAttribute('date', `${year}-${month}-${day}`);
-  })
-};
-
-function parseNumber(number) {
-  return number < 10 ? `0${number}` : number
-};
+});
 
 async function getData() {
   try {
-    const res = await fetch('https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/users?limit=15')
-    const res1 = await fetch('https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/tasks')
-    const tasks = await res1.json()
-    const users = await res.json()
+    const users_result = await fetch(`${BASE_URL}users?limit=15`)
+    const tasks_result = await fetch(`${BASE_URL}/tasks`)
+    const tasks = await tasks_result.json()
+    const users = await users_result.json()
 
     const activeTasks = []
     const backlog = []
@@ -117,7 +91,7 @@ async function getData() {
       return { ...user, tasks }
     })
     state.backlog = backlog
-    setDate(new Date())
+    setWeek(new Date())
     updateCells()
     updateBacklog()
     document.querySelector('#wrapper').classList.remove('hidden')
@@ -129,32 +103,57 @@ async function getData() {
   }
 };
 
+getData()
+
+function changeDayDate(date, n) {
+  date.setDate(date.getDate() + n);
+  return date;
+};
+
+function setWeek(date) {
+  const week = date.getDay() - 1;
+  const newDate = changeDayDate(date, week * -1);
+  state.currentFirstDate = new Date(newDate);
+
+  Array.from(THS).forEach((th, i) => {
+    if (i === 0) return
+
+    const curDate = i === 1 ? newDate : changeDayDate(newDate, 1)
+    const month = parseNumber(curDate.getMonth() + 1)
+    const day = parseNumber(curDate.getDate())
+    const year = newDate.getFullYear()
+    th.innerHTML = `${day}.${month}`
+    th.setAttribute('date', `${year}-${month}-${day}`);
+  })
+};
+
+function parseNumber(number) {
+  return number < 10 ? `0${number}` : number
+};
+
 function updateCells() {
   SCHEDULER_TABLE_BODY.innerHTML = ''
 
   state.users.forEach(user => {
     const row = document.createElement('tr')
-    const td = document.createElement('td')
-    td.setAttribute('type', 'username')
-    td.setAttribute('userId', user.id)
-    td.setAttribute('class', 'cell-username')
-    td.innerHTML = user.username
-    row.append(td)
+    const cellName = document.createElement('td')
+    cellName.setAttribute('type', 'username')
+    cellName.setAttribute('user-id', user.id)
+    cellName.setAttribute('class', 'cell-username')
+    cellName.innerHTML = user.username
+    row.append(cellName)
 
     Array.from(THS).forEach((th, i) => {
       if (i === 0) return
       const userTasks = user.tasks[th.getAttribute('date')]
       const cell = document.createElement('td');
       cell.setAttribute('date', th.getAttribute('date'))
-      cell.setAttribute('userId', user.id)
+      cell.setAttribute('user-id', user.id)
       cell.setAttribute('type', 'task')
       cell.setAttribute('class', 'cell-task')
       if (userTasks) {
         cell.setAttribute('class', 'cell-task_active')
-        cell.innerHTML = `<div class="task-container">
-        ${userTasks.map((item, i) => {
-          return `<div class="task"><span class="tooltip" data-tooltip="${item.subject}">Задача ${i + 1}</span></div>`
-        }).join('')}</div>`
+        cell.innerHTML = getUserTasksTemplate(userTasks)
       }
       row.append(cell)
     })
@@ -177,6 +176,15 @@ function updateBacklog() {
   })
 };
 
-
-getData()
+function getUserTasksTemplate(userTasks) {
+  return `
+    <div class="task-container">
+      ${userTasks.map((item, i) => `
+      <div class="task">
+        <span class="tooltip" data-tooltip="${item.subject}">
+          Задача ${i + 1}
+        </span>
+      </div>`).join('')}
+    </div>`
+};
 
